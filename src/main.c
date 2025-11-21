f5a700e56fb00a898784ccdefd1a58b0493f6f31
Piecuuu <56731916+Piecuuu@users.noreply.github.com>
2025-11-21 23:04:19 +0100

    main: create boilerplate code

--- src/main.c
+++ src/main.c
@@ -3,8 +3,20 @@
  * See LICENSE in the root of the repository for details.
  */
 
+#include <concord/discord.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int main() {
+  const char* token = getenv("BOT_TOKEN");
+  if(!token || strlen(token) == 0) {
+    fprintf(stderr, "BOT_TOKEN is missing.\n");
+    return 1;
+  }
+
+  struct discord* client = discord_from_token(token);
+  
+  discord_run(client);
   return 0;
 }
