3f2746529928ee4b47b8356efc187a07b476c89a
Piecuuu <56731916+Piecuuu@users.noreply.github.com>
2025-11-23 17:15:51 +0100

    db: add license header and fix header

--- src/db.c
+++ src/db.c
@@ -1,4 +1,10 @@
+/*
+ * This code is licensed under the European Union Public Licence, Version 1.2
+ * See LICENSE in the root of the repository for details.
+ */
+
 #include "db.h"
+#include <stddef.h>
 #include <concord/logmod.h>
 
 sqlite3* am_db = NULL;
@@ -6,9 +12,10 @@ sqlite3* am_db = NULL;
 int am_db_init(const char* db_path) {
   int rc = sqlite3_open(db_path, &am_db);
   if(rc != SQLITE_OK) {
-    logmod_log(ERROR, NULL, "Failed to open database: %s\n", sqlite3_errmsg(am_db));
+    logmod_log(ERROR, NULL, "Failed to open database: %s", sqlite3_errmsg(am_db));
     sqlite3_close(am_db);
     return 1;
   }
+  logmod_log(INFO, NULL, "Succesfully opened database");
   return 0;
 }
--- src/db.h
+++ src/db.h
@@ -1,8 +1,15 @@
+/*
+ * This code is licensed under the European Union Public Licence, Version 1.2
+ * See LICENSE in the root of the repository for details.
+ */
+
 #ifndef AM_DB_H
 #define AM_DB_H
 
 #include <sqlite3.h>
 
 extern sqlite3* am_db;
+
+int am_db_init(const char* db_path);
 
 #endif // AM_DB_H
